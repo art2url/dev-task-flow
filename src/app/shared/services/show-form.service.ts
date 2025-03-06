a08abf9ed42ce7f7893471971c2f44367eb3bbce
Artem Turlenko <76534386+art2url@users.noreply.github.com>
2025-03-06 10:35:59 +0100

    Added form visibility toggle persistence

--- src/app/shared/services/show-form.service.ts
+++ src/app/shared/services/show-form.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ShowFormService {
   private showFormSubject = new BehaviorSubject<boolean>(true);
-  showForm$ = this.showFormSubject.asObservable();
+
+  get showForm$(): Observable<boolean> {
+    return this.showFormSubject.asObservable();
+  }
 
   toggleForm(): void {
     this.showFormSubject.next(!this.showFormSubject.value);
