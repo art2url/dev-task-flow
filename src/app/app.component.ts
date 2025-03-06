d5d2e824cbec0932ef2c56562976e5893288428c
Artem Turlenko <76534386+art2url@users.noreply.github.com>
2025-03-06 10:36:31 +0100

    Minor refactor for clarity

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { HeaderComponent } from './shared/header/header.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
